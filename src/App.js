c90d6bab6a00975311864a929bfdcd789fbe18c9
Dharaneesh D <dharaneesh8688@gmail.com>
2024-06-16 14:27:04 +0530

    update

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
     <div>
       <input type="button" value="00"  onClick={e => setValue(value + e.target.value)} />
       <input type="button" value="0"  onClick={e => setValue(value + e.target.value)} />
-      <input className='equal' type="button" value="=" onClick={e => setValue(eval(value))} />
+      <input className='equal' type="button" value="=" onClick={e => setValue((value))} />
     </div>
   </form>
 </div>
